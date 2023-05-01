b1030cd948804665f5b4e88faea7c5dc88241690
kodyka <131517845+kodyka@users.noreply.github.com>
2023-05-01 16:43:04 -0400

    Update wait_for_postgres.py

--- utils/wait_for_postgres.py
+++ utils/wait_for_postgres.py
@@ -7,10 +7,11 @@ import os
 if __name__ == "__main__":
     started_at = datetime.utcnow()
     postgres_host = os.environ.get("POSTGRES_HOST", "postgres")
+    postgres_port = os.environ.get("POSTGRES_PORT", 5432)
     while datetime.utcnow() < started_at + timedelta(minutes=5):
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                sock.connect((postgres_host, 5432))
+                sock.connect((postgres_host, postgres_port))
                 print("Postgres had started")
                 break
         except socket.error:
